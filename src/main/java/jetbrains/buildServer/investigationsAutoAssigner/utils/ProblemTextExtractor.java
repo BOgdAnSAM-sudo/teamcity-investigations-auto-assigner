9cef0fe5a5af5bf3a5deae594f07214e443dc589
BOgdAnSAM-sudo <samojlenkobogdandmitrovic@gmail.com>
2025-04-05 14:33:21 +0200

    enhance ProblemTextExtractor for using extensions

--- src/main/java/jetbrains/buildServer/investigationsAutoAssigner/utils/ProblemTextExtractor.java
+++ src/main/java/jetbrains/buildServer/investigationsAutoAssigner/utils/ProblemTextExtractor.java
@@ -1,59 +1,49 @@
-
-
 package jetbrains.buildServer.investigationsAutoAssigner.utils;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import jetbrains.buildServer.BuildProblemTypes;
-import jetbrains.buildServer.investigationsAutoAssigner.common.Constants;
 import jetbrains.buildServer.serverSide.SBuild;
 import jetbrains.buildServer.serverSide.STest;
 import jetbrains.buildServer.serverSide.STestRun;
-import jetbrains.buildServer.serverSide.TeamCityProperties;
-import jetbrains.buildServer.serverSide.buildLog.LogMessage;
-import jetbrains.buildServer.serverSide.problems.BuildLogCompileErrorCollector;
 import jetbrains.buildServer.serverSide.problems.BuildProblem;
 import jetbrains.buildServer.tests.TestName;
-import jetbrains.buildServer.util.ItemProcessor;
-import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-import static jetbrains.buildServer.serverSide.impl.problems.types.CompilationErrorTypeDetailsProvider.COMPILE_BLOCK_INDEX;
 
 public class ProblemTextExtractor {
-  public String getBuildProblemText(@NotNull final BuildProblem problem, @NotNull final SBuild build) {
-    StringBuilder problemSpecificText = new StringBuilder();
+  private final List<ProblemTextExtractorExtension> extensions = new CopyOnWriteArrayList<>();
 
-    // todo make an extension point here
-    if (problem.getBuildProblemData().getType().equals(BuildProblemTypes.TC_COMPILATION_ERROR_TYPE)) {
-      final Integer compileBlockIndex = getCompileBlockIndex(problem);
-      if (compileBlockIndex != null) {
-        AtomicInteger maxErrors = new AtomicInteger(TeamCityProperties.getInteger(Constants.MAX_COMPILE_ERRORS_TO_PROCESS, 100));
-        BuildLogCompileErrorCollector.collectCompileErrors(compileBlockIndex, build, new ItemProcessor<LogMessage>() {
-          @Override
-          public boolean processItem(final LogMessage item) {
-            problemSpecificText.append(item.getText()).append(" ");
-            return maxErrors.decrementAndGet() > 0;
-          }
-        });
+  public ProblemTextExtractor() {
+  }
+
+  public ProblemTextExtractor(@NotNull List<ProblemTextExtractorExtension> extensions) {
+    this.extensions.addAll(extensions);
+  }
+
+  public List<ProblemTextExtractorExtension> getExtensions() {
+    return extensions;
+  }
+
+  public void registerExtension(@NotNull ProblemTextExtractorExtension extension) {
+    extensions.add(extension);
+  }
+
+  public void unregisterExtension(@NotNull ProblemTextExtractorExtension extension) {
+    extensions.remove(extension);
+  }
+
+  public String getBuildProblemText(@NotNull final BuildProblem problem, @NotNull final SBuild build) {
+    for (ProblemTextExtractorExtension extension : extensions) {
+      if (extension.supports(problem)) {
+        String text = extension.extractText(problem, build);
+        if (text != null) {
+          return text;
+        }
+        break;
       }
     }
 
-    return problemSpecificText + " " + problem.getBuildProblemDescription();
-  }
-
-  @Nullable
-  private static Integer getCompileBlockIndex(@NotNull final BuildProblem problem) {
-    final String compilationBlockIndex = problem.getBuildProblemData().getAdditionalData();
-    if (compilationBlockIndex == null) return null;
-
-    try {
-      return Integer.parseInt(
-        StringUtil.stringToProperties(compilationBlockIndex, StringUtil.STD_ESCAPER2).get(COMPILE_BLOCK_INDEX));
-    } catch (Exception e) {
-      return null;
-    }
+    return problem.getBuildProblemDescription();
   }
 
   public String getBuildProblemText(STestRun sTestRun) {
@@ -61,4 +51,5 @@ public class ProblemTextExtractor {
     final TestName testName = test.getName();
     return testName.getAsString() + " " + sTestRun.getFullText();
   }
+
 }
